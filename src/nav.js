e06484d9d3a4167da1e900d8225b021b5fe74fc7
Becky449 <rabnab12@gmail.com>
2023-08-16 19:04:13 +0300

    updated navigation

--- src/nav.js
+++ src/nav.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { FaTruckMoving } from 'react-icons/fa';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { BsBagCheck } from 'react-icons/bs';
+import { AiOutlineUser } from 'react-icons/ai';
 import './nav.css';
 const Nav = () => {
   return (
@@ -12,6 +15,20 @@ const Nav = () => {
         <div className="container">
             <div className="logo">
               <img src="./img/logo.svg" alt="logo" />
+            </div>
+            <div className='search_box'>
+              <input type="text" placeholder="Search Your Product..." autoComplete='off'/>
+              <button className="btn">Search</button>
+            </div>
+            <div className="icon">
+              <div className="account">
+                <div className="user_icon">
+                <AiOutlineUser />
+                </div>
+                <p>Hello, user</p>
+              </div>
+              <AiOutlineHeart />
+              <BsBagCheck />
             </div>
         </div>
     </div>
